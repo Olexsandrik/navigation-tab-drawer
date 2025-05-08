dad3ea71d4d2333f3be0e494e7e3d44bba8d49ca
Olexsandrik <119703838+Olexsandrik@users.noreply.github.com>
2025-05-08 22:24:41 +0300

    ADD GAME FOR LEARNING WORDS

--- screens/Learning/Play.js
+++ screens/Learning/Play.js
@@ -7,49 +7,88 @@ import { COLORS } from "../../constants";
 import { playSound } from "../../services/soundHandler";
 export default function Play() {
   const [data, setData] = useState(MyWords);
-
+  const [idx, setIndex] = useState(0);
   const [show, setShow] = useState(false);
   const handlerStudy = () => {
-    console.log("Pressed!");
     setShow(true);
   };
+
+  const handlerDidntKnow = () => {
+    if (idx < data.length - 1) {
+      setIndex(idx + 1);
+      setShow(false);
+    }
+  };
+
+  const handlerKnowIt = () => {
+    setData((prev) =>
+      prev.map((item, index) => {
+        return index === idx ? { ...item, status: item.status + 1 } : item;
+      })
+    );
+    setIndex(idx < data.length - 1 ? idx + 1 : 0);
+  };
+
+  const checkStatus = data.every((item) => item.status === 2);
+
+  console.log(data[idx].status, idx, data[idx].word);
   return (
     <ScrollView>
-      <View style={[styles.dataWordsInfo, { minHeight: show ? 550 : 650 }]}>
-        <Pressable onPress={handlerStudy}>
-          <Text style={styles.textMain}>{data[0].word}</Text>
-        </Pressable>
-        {show ? (
-          <View style={styles.additionData}>
-            <Text style={styles.textPhonetic}>{data[0].phonetics}</Text>
-            <Pressable
-              disabled={!data[0].audio}
-              onPress={() => playSound(data[0].audio)}
-            >
-              <View>
-                <Ionicons
-                  name="play-outline"
-                  size={28}
-                  style={[
-                    !data[0].audio
-                      ? { color: COLORS.grey300 }
-                      : { color: COLORS.primary900 },
-                    { marginTop: "20" },
-                  ]}
-                />
-              </View>
-            </Pressable>
-            <Text style={styles.textMeaning}>{data[0].meaning}</Text>
-          </View>
-        ) : null}
-      </View>
-
-      {show ? (
-        <View style={styles.btnContainer}>
-          <Button title="Didn't know it" />
-          <Button title="Knew it" />
+      {checkStatus ? (
+        <View
+          style={{
+            justifyContent: "center",
+            alignContent: "center",
+            minHeight: "50%",
+            maxWidth: "80%",
+            margin: "auto",
+          }}
+        >
+          <Text style={{ color: "#ffff" }}>Congrats!</Text>
+          <Text style={{ color: "#ffff", marginTop: 20 }}>
+            For now you have learned all the words should be printed on the
+            screen
+          </Text>
         </View>
-      ) : null}
+      ) : (
+        <View>
+          <View style={[styles.dataWordsInfo, { minHeight: show ? 550 : 650 }]}>
+            <Pressable onPress={handlerStudy}>
+              <Text style={styles.textMain}>{data[idx].word}</Text>
+            </Pressable>
+            {show ? (
+              <View style={styles.additionData}>
+                <Text style={styles.textPhonetic}>{data[idx].phonetics}</Text>
+                <Pressable
+                  disabled={!data[idx].audio}
+                  onPress={() => playSound(data[idx].audio)}
+                >
+                  <View>
+                    <Ionicons
+                      name="play-outline"
+                      size={28}
+                      style={[
+                        !data[idx].audio
+                          ? { color: COLORS.grey300 }
+                          : { color: COLORS.primary900 },
+                        { marginTop: "20" },
+                      ]}
+                    />
+                  </View>
+                </Pressable>
+                <Text style={styles.textMeaning}>{data[idx].meaning}</Text>
+              </View>
+            ) : null}
+          </View>
+
+          {show ? (
+            <View style={styles.btnContainer}>
+              <Button title="Didn't know it" onPress={handlerDidntKnow} />
+              <Button title="Knew it" onPress={handlerKnowIt} />
+            </View>
+          ) : null}
+        </View>
+      )}
     </ScrollView>
   );
 }
@@ -65,12 +104,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   textPhonetic: {
-    marginTop: "20",
+    marginTop: 20,
     color: "#ffff",
   },
 
   textMeaning: {
-    marginTop: "20",
+    marginTop: 20,
     color: "#ffff",
   },
 
